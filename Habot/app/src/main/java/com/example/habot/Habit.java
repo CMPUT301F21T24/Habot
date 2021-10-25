4522620e6620fc295c5eee85dec38e667bc44c8d
HaoweiYu <43255180+HaoweiYu@users.noreply.github.com>
2021-10-24 22:41:03 -0600

    Update Habit.java

--- Habot/app/src/main/java/com/example/habot/Habit.java
+++ Habot/app/src/main/java/com/example/habot/Habit.java
@@ -1,4 +1,33 @@
 package com.example.habot;
 
 public class Habit {
+    private Stirng title;
+    private String reason;
+    private Date date;
+
+    Habit(String title, Stirng Reason, Date date)
+    {
+        this.title = title;
+        this.reason = reason;
+        this.date = date;
+    }
+
+    public String gettitle(){
+        return this.title;
+    }
+    public String getreason(){
+        return this.reason;
+    }
+    public String getdate(){
+        return this.date;
+    }
+    public String settitle(String title){
+        this.title = title;
+    }
+    public String setreason(String reason){
+        this.reason = reason;
+    }
+    public String setdate(String date){
+        this.date = date;
+    }
 }
