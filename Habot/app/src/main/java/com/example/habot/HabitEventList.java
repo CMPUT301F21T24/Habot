78e9a82cd31946c6ad8238985216f2073f4a8b53
KexunNiu <77284970+KexunNiu@users.noreply.github.com>
2021-10-24 22:53:29 -0600

    Update HabitEventsList.java
    Add setters and getters.

--- Habot/app/src/main/java/com/example/habot/HabitEventList.java
+++ Habot/app/src/main/java/com/example/habot/HabitEventList.java
@@ -1,4 +1,27 @@
 package com.example.habot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HabitEventList {
+  private List<Habit_Event> HabitEventsList = new ArrayList<>();
+
+    /**
+     * This add a habitEvent to the habitEventList
+     * @param habit_event
+     *   this is a argument added to the habitEventList
+     */
+    public void addHabitEvent(Habit_Event habit_event){
+        HabitEventsList.add(habit_event);
+    }
+
+    /**
+     * this returns the HabitEventLists
+     * @return
+     *      it takes no argument and return a list
+     */
+    public List<Habit_Event> getHabitEvents(){
+        List<Habit_Event> list = HabitEvents;
+        return list;
+    }
 }
