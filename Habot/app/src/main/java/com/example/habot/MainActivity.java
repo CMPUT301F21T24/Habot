0eaa7726f7176f5fe5f644efdd689891a7a1654e
KexunNiu <77284970+KexunNiu@users.noreply.github.com>
2021-11-01 21:01:31 -0600

    Cover the old Habot
     update the habot with Firebase

--- Habot/app/src/main/java/com/example/habot/MainActivity.java
+++ Habot/app/src/main/java/com/example/habot/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseFirestore db
     }
 }
