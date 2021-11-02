c8b5da053b35d652b4dc8711ed6e88448ffc19ba
Wenhao Cao <wcao4@ualberta.ca>
2021-11-02 00:35:42 -0600

    ADD

--- Habot/app/src/main/java/com/example/habot/RequestRecieveList.java
+++ Habot/app/src/main/java/com/example/habot/RequestRecieveList.java
@@ -1,4 +1,16 @@
 package com.example.habot;
 
+import java.util.ArrayList;
+
 public class RequestRecieveList {
+    private ArrayList<Request> requestArrayList;
+
+    public void addRequest(Request request){
+        requestArrayList.add(request);
+    }
+
+    public ArrayList<Request> returnRequestList(){
+        return this.requestArrayList;
+
+    }
 }
