f8db212631a5d7f9ff73f1dfe8486f93fc0f1d83
kkli08 <ke_2021@126.com>
2021-11-24 07:09:49 -0700

    update by kli1 7:09

--- Habot/app/src/main/java/com/example/habot/Habitlist.java
+++ Habot/app/src/main/java/com/example/habot/Habitlist.java
@@ -1,23 +1,26 @@
 package com.example.habot;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
-        /**
+/**
         * This is a Habitlist that extends from ArrayAdapter
         */
 public class Habitlist extends ArrayAdapter<Habit> {
     public ArrayList<Habit> habits;
-
     private Context context;
 
     /**
@@ -42,15 +45,76 @@ public class Habitlist extends ArrayAdapter<Habit> {
      */
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-// return super.getView(position, convertView, parent);
         View view = convertView;
         if(view == null){
             view = LayoutInflater.from(context).inflate(R.layout.habitdetailcontent, parent,false);
         }
         Habit habit = habits.get(position);
         TextView habitname = view.findViewById(R.id.Habit_text);
-
+        ProgressBar progressBar = view.findViewById(R.id.progressBar);
+        TextView textView = view.findViewById(R.id.text_view);
         habitname.setText(habit.gettitle());
+        String week = habit.getTime();
+        Log.d("TAG", "getView: ------------------>>>>>weekkkkk"+week);
+
+        int Today_day = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
+        int habit_total_days = 0;
+        int habit_occur = 0;
+        if (week != null) {
+            if (week.charAt(0) == '1') {
+                habit_total_days += 1;
+                if (Today_day >= 1) {
+                    habit_occur += 1;
+                }
+            }
+            if (week.charAt(1) == '1') {
+                habit_total_days += 1;
+                if (Today_day >= 2) {
+                    habit_occur += 1;
+                }
+            }
+            if (week.charAt(2) == '1') {
+                habit_total_days += 1;
+                if (Today_day >= 3) {
+                    habit_occur += 1;
+                }
+            }
+            if (week.charAt(3) == '1') {
+                habit_total_days += 1;
+                if (Today_day >= 4) {
+                    habit_occur += 1;
+                }
+            }
+            if (week.charAt(4) == '1') {
+                habit_total_days += 1;
+                if (Today_day >= 5) {
+                    habit_occur += 1;
+                }
+            }
+            if (week.charAt(5) == '1') {
+                habit_total_days += 1;
+                if (Today_day >= 6) {
+                    habit_occur += 1;
+                }
+            }
+            if (week.charAt(6) == '1') {
+                habit_total_days += 1;
+                if (Today_day >= 7) {
+                    habit_occur += 1;
+                }
+            }
+        }
+            float percent = ((float)habit_occur/(float)habit_total_days)*100;
+            Log.d("TAG", "getView: -------------->>>>>>habit_occur %%%%%%%%-->>>>"+ habit_occur);
+            Log.d("TAG", "getView: -------------->>>>>>habit_total_days %%%%%%%%-->>>>"+ habit_total_days);
+            int progressPercent = (int)percent;
+            String habit_finished = Integer.toString(progressPercent);
+            textView.setText(habit_finished+'%');
+            Log.d("TAG", "getView: -------------->>>>>>int progressPercent %%%%%%%%-->>>>"+ progressPercent);
+            progressBar.setProgress(progressPercent);
+
+
+
 
         return view;
     }
